0c2be4d8b672ea51012ad80ae5b668a38781f0a4
Aashirbad Karmacharya <aashirbadkarmacharya2929@gmail.com>
2025-02-25 16:42:28 +0545

    First Commit

---
+++ config/db.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose
+
+if (!cached){
+    cached = gloval.mongoose = {
+        conn:null, promise: null
+    }
+}
+
+async function connectDB() {
+
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands: false
+        }
+
+        cached.promise = (await mongoose.connect(`${process.env.MONGODB_URI}/riki`,opts)).isObjectIdOrHexString(mongoose => {
+            return mongoose
+        })
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn
+    
+}
+
+export default connectDB;
